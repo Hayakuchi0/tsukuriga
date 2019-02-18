db90ef55011811e5ed150fb05e3f2f7ed379a4ae
Compeito <com0806peito@icloud.com>
2019-02-19 00:24:52 +0900

    jquery削除

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,30 +1,16 @@
 import './styles.scss'
 
-$(document).ready(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  const $ = q => document.querySelectorAll(q)
 
   /**
    * メニュー開閉
    * https://bulma.io/documentation/components/navbar/#navbar-menu
    */
-  $('.navbar-burger').click(() => {
-    $('.navbar-burger').toggleClass('is-active')
-    $('.navbar-menu').toggleClass('is-active')
+  const $navbarBurger = $('.navbar-burger')[0]
+  $navbarBurger.addEventListener('click', (e) => {
+    $('.navbar-burger')[0].classList.toggle('is-active')
+    $('.navbar-menu')[0].classList.toggle('is-active')
   })
-
-  /**
-   * data-counted属性の付いたフォームに文字数カウンタ追加
-   * /create
-   */
-  $('*[data-counted]')
-    .after(
-      `<div class="counter has-text-right"> </div>`
-    )
-    .on('keydown keyup keypress change', function() {
-      const strLength = $(this).val().length
-      const stateText =
-        strLength !== 0 ?
-          strLength + '/' + $(this).attr('maxlength') : ''
-      $(this).next('.counter').text(stateText)
-    })
 
 })
