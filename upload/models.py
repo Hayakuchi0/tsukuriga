f7c2ad1fe1e12e1b3567e5c9edb6a112c1d8e030
Compeito <com0806peito@icloud.com>
2019-03-06 01:09:41 +0900

    Videoモデル基礎作成

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from uuid import uuid4
+from pathlib import Path
 
-# Create your models here.
+from django.db import models
+from core.utils import CustomModel, gen_unique_slug
+
+
+def default_video_slug():
+    return gen_unique_slug(5, Video)
+
+
+def video_upload_to(instance: 'Video', filename):
+    return Path(instance.user.username) / 'videos' / instance.slug / f'{uuid4().hex}.mp4'
+
+
+def thumbnail_upload_to(instance: 'Video', filename):
+    return Path(instance.user.username) / 'videos' / instance.slug / f'{uuid4().hex}.jpg'
+
+
+class Video(models.Model):
+    user = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    slug = models.CharField(max_length=5, default=default_video_slug)
+    file = models.FileField(upload_to=video_upload_to)
+    fps = models.PositiveIntegerField()
+    duration = models.FloatField()
+    thumbnail = models.ImageField(upload_to=thumbnail_upload_to)
+
+
+class VideoProfile(CustomModel):
+    video = models.OneToOneField(Video, on_delete=models.CASCADE, related_name='profile')
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    # channel = models.PositiveIntegerField
+    # tags = models.PositiveIntegerField
