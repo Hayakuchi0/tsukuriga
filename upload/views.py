8d76f99eba9bed9108099282e70676b993ab37ec
Compeito <com0806peito@icloud.com>
2019-03-06 17:14:52 +0900

    アップロードフォーム更新

--- upload/views.py
+++ upload/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 
+def get_process(active_index):
+    process = [
+        {'title': 'ファイル選択', 'is_active': False},
+        {'title': '紹介文設定', 'is_active': False},
+        {'title': '完了！', 'is_active': False},
+    ]
+    process[active_index]['is_active'] = True
+    return process
+
+
 def upload(request):
-    return render(request, 'upload/form.html')
+    process = get_process(0)
+    return render(request, 'upload/form.html', {'process': process})
