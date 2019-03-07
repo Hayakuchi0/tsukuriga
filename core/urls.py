d27e3ad1bf80d8fd181c91403b06516ff51bb38f
Compeito <com0806peito@icloud.com>
2019-03-07 23:41:55 +0900

    視聴ページ基礎作成

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.top)
+    path('', views.top),
+    path('watch/<slug:slug>', views.watch),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from upload.models import Video
 
 
 def top(request):
     return render(request, 'core/top.html')
+
+
+def watch(request, slug):
+    video = get_object_or_404(Video, slug=slug)
+    return render(request, 'core/watch.html', {'video': video})
