938292790d0912d13265a9e1072d83a079db4302
Compeito <com0806peito@icloud.com>
2019-03-11 00:31:35 +0900

    動画サムネイルリストビュー基礎作成

--- browse/views.py
+++ browse/views.py
@@ -1,13 +1,13 @@
-from upload.models import Video
+from upload.models import VideoData
 from core.utils import AltPaginationListView
 
 
 class Browse(AltPaginationListView):
     template_name = 'browse/index.html'
-    context_object_name = 'videos'
+    context_object_name = 'video_data'
 
     def get_queryset(self):
-        return Video.objects.all().order_by('-profile__created_at')
+        return VideoData.objects.all().order_by('-video__profile__created_at')
 
 
 browse = Browse.as_view()
