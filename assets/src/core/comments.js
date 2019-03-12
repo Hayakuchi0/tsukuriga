68c978d98c3cf5612abd8b6512bd2085db2aef11
Compeito <com0806peito@icloud.com>
2019-03-12 22:20:28 +0900

    コメント一覧表示基礎作成

--- assets/src/core/comments.js
+++ assets/src/core/comments.js
@@ -1,34 +1,76 @@
 import axios from 'axios'
 import Notyf from 'notyf'
+import Vue from 'vue/dist/vue'
 
-import { doc } from '../utils'
+import { doc, ready } from '../utils'
 
 
-const $ajaxForm = doc('form.ajax-form')
-$ajaxForm.forEach($form => {
-  const $submitButton = $form.querySelector('button[type=submit]')
-  $form.addEventListener('submit', e => {
-    e.preventDefault()
+ready(() => {
+  /**
+   * コメント単体のVueコンポーネント
+   */
+  Vue.component('v-comment-item', {
+    template: '#v-comment-item',
+    delimiters: ['[[', ']]'],
+    props: ['comment']
+  })
 
-    $form.reset()
-    $submitButton.disabled = true
-    $submitButton.classList.add('is-loading')
-
-    const formData = new FormData($form)
-    axios.post($form.target, formData)
-      .then(response => {
-        const notyf = new Notyf({
-          delay: 5000
-        })
-        if (response.data.isSuccess) {
-          response.data.results.forEach(result => {
-            notyf.confirm(result.message)
+  /**
+   * コメント一覧のVueコンポーネント
+   */
+  const commentList = new Vue({
+    el: '#v-comment-list',
+    data() {
+      return {
+        comments: []
+      }
+    },
+    mounted() {
+      this.getComments()
+    },
+    methods: {
+      getComments() {
+        const videoId = doc('video')[0].dataset.videoId
+        const self = this
+        axios.get(`/ajax/comments/list/${videoId}`)
+          .then(response => {
+            self.comments = response.data.results
           })
-        }
-      })
-      .finally(() => {
-        $submitButton.classList.remove('is-loading')
-        $submitButton.disabled = false
-      })
+      }
+    }
+  })
+
+  /**
+   * コメントフォーム
+   */
+  const $ajaxForm = doc('form.ajax-form')
+  $ajaxForm.forEach($form => {
+    const $submitButton = $form.querySelector('button[type=submit]')
+    $form.addEventListener('submit', e => {
+      e.preventDefault()
+
+      $submitButton.disabled = true
+      $submitButton.classList.add('is-loading')
+
+      const formData = new FormData($form)
+      $form.reset()
+
+      axios.post($form.target, formData)
+        .then(response => {
+          const notyf = new Notyf({
+            delay: 5000
+          })
+          if (response.data.isSuccess) {
+            response.data.results.forEach(result => {
+              notyf.confirm(result.message)
+            })
+          }
+        })
+        .finally(() => {
+          commentList.getComments()
+          $submitButton.classList.remove('is-loading')
+          $submitButton.disabled = false
+        })
+    })
   })
 })
--- assets/src/utils.js
+++ assets/src/utils.js
@@ -1,12 +1,3 @@
-import Vue from 'vue/dist/vue'
-
 export const ready = callback => document.addEventListener('DOMContentLoaded', callback)
-
-export const vue_component = options => {
-  ready(() => {
-    options.delimiters = ['[[', ']]']
-    new Vue(options)
-  })
-}
 
 export const doc = q => document.querySelectorAll(q)
