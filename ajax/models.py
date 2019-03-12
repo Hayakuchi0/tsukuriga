7b061b477740696ec39943f2463cc17d53c47315
Compeito <com0806peito@icloud.com>
2019-03-12 17:54:17 +0900

    Commentモデル追加

--- ajax/models.py
+++ ajax/models.py
@@ -1,1 +1,9 @@
+from django.db import models
+from django.utils import timezone
 
+
+class Comment(models.Model):
+    user = models.ForeignKey('account.User', verbose_name='ユーザー', on_delete=models.CASCADE)
+    video = models.ForeignKey('upload.Video', verbose_name='動画', on_delete=models.CASCADE)
+    text = models.TextField('本文', max_length=200)
+    created_at = models.DateTimeField('作成日', default=timezone.now)
