66e2d760143c62bc9a0d00eed311ac28132d4265
Compeito <com0806peito@icloud.com>
2019-03-12 21:47:26 +0900

    コメントリストAPI追加

--- ajax/urls.py
+++ ajax/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('comments/add/<slug:slug>', views.add_comment),
+    path('comments/list/<slug:slug>', views.list_comments),
 ]
--- ajax/views.py
+++ ajax/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_GET, require_POST
 
 from upload.models import Video
+from .models import Comment
 from .forms import CommentForm
 from .utils import json_response
 
@@ -9,10 +11,7 @@ from .utils import json_response
 @require_POST
 @login_required
 def add_comment(request, slug):
-    try:
-        video = Video.objects.get(slug=slug)
-    except:
-        return json_response([{'message': '動画が存在しません'}], status=400)
+    video = get_object_or_404(Video, slug=slug)
 
     form = CommentForm(request.POST)
     if form.is_valid():
@@ -23,3 +22,10 @@ def add_comment(request, slug):
         return json_response([{'message': 'コメントが追加されました'}], status=200)
 
     return json_response(form.errors, status=400)
+
+
+@require_GET
+def list_comments(request, slug):
+    video = get_object_or_404(Video, slug=slug)
+    comments = Comment.objects.filter(video=video)
+    return json_response([comment.json() for comment in comments])
