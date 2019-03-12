919eceaad69d612eb0bef6aff08f2129e0bc878e
Compeito <com0806peito@icloud.com>
2019-03-12 17:55:21 +0900

    コメント追加API基礎作成

--- ajax/urls.py
+++ ajax/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+
+from . import views
+
 urlpatterns = [
-    # path(''),
+    path('comment/add/<slug:slug>', views.add_comment),
 ]
--- ajax/views.py
+++ ajax/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
+from upload.models import Video
+from .forms import CommentForm
+from .utils import json_response
+
+
+@require_POST
+@login_required
+def add_comment(request, slug):
+    try:
+        video = Video.objects.get(slug=slug)
+    except:
+        return json_response([{'message': '動画が存在しません'}], status=400)
+
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.video = video
+        comment.save()
+        return json_response([{'message': 'コメントが追加されました'}], status=200)
+
+    return json_response(form.errors, status=400)
