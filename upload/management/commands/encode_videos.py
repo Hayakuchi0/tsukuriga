d3b53dec67a0cbdf8bab851fadc3d5e4fb4efc0d
Compeito <com0806peito@icloud.com>
2019-03-14 22:52:47 +0900

    エンコード失敗時用処理追加

--- upload/management/commands/encode_videos.py
+++ upload/management/commands/encode_videos.py
@@ -1,3 +1,5 @@
+import traceback
+
 from django.core.management.base import BaseCommand
 from upload.models import UploadedPureVideo
 
@@ -8,4 +10,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         videos = UploadedPureVideo.objects.all().order_by('-created_at')
         for video in videos:
-            video.make()
+            if video.is_encoding:
+                continue
+            try:
+                video.make()
+            except:
+                video.is_failed = True
+                video.traceback = traceback.format_exc()
+                video.save()
