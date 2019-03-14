dd50a2277e00b7643f1dd81ce52e377b46a3f783
Compeito <com0806peito@icloud.com>
2019-03-14 21:44:09 +0900

    videojs日本語化

--- assets/src/core/watch.js
+++ assets/src/core/watch.js
@@ -1,8 +1,12 @@
 import videojs from 'video.js'
+import lang_ja from 'video.js/dist/lang/ja.json'
 
 import { ready } from '../utils'
 import './styles.scss'
 
 ready(() => {
+  videojs.addLanguage('ja', Object.assign(lang_ja, {
+    'No compatible source was found for this media.': '現在エンコード処理中のため閲覧できません'
+  }))
   const player = videojs('video-player')
 })
