8cab0202a6cdebed16a4048c6738e62e1ea08c95
Compeito <com0806peito@icloud.com>
2019-03-14 15:28:07 +0900

    videoコンポーネント化に伴う修正

--- browse/views.py
+++ browse/views.py
@@ -4,10 +4,10 @@ from core.utils import AltPaginationListView
 
 class Browse(AltPaginationListView):
     template_name = 'browse/index.html'
-    context_object_name = 'video_data'
+    context_object_name = 'videos'
 
     def get_queryset(self):
-        return VideoData.objects.all().order_by('-video__profile__created_at')
+        return [v.video for v in VideoData.objects.all().order_by('-video__profile__created_at')]
 
 
 browse = Browse.as_view()
