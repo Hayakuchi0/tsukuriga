8a0d3273e2a0f29800d08bd65212a7637cfe2a5c
Compeito <com0806peito@icloud.com>
2019-03-16 16:35:37 +0900

    コマ送り機能追加

--- assets/src/core/watch.js
+++ assets/src/core/watch.js
@@ -1,12 +1,49 @@
-import videojs from 'video.js'
-import lang_ja from 'video.js/dist/lang/ja.json'
+import { doc, ready, range } from '../utils'
 
-import { ready } from '../utils'
-import './styles.scss'
+
+class Video {
+  constructor(video) {
+    this.$el = doc(video)[0]
+    const fps = parseInt(this.$el.dataset.fps)
+    const duration = parseFloat(this.$el.dataset.duration)
+    const frameLength = 1 / fps
+    const framesCount = Math.floor(fps * duration)
+    this.frames = range(framesCount).map(i => i * frameLength)
+  }
+
+  get currentFrame() {
+    let minDiffFrame = 0
+    let diff = []
+    this.frames.forEach((frameVal, frame) => {
+      diff[frame] = Math.abs(this.$el.currentTime - frameVal)
+      minDiffFrame = (diff[minDiffFrame] < diff[frame]) ? minDiffFrame : frame
+    })
+    return minDiffFrame
+  }
+
+  ready() {
+    if (this.$el.currentTime === 0) this.$el.play()
+    if (!this.$el.paused) this.$el.pause()
+    return this
+  }
+
+  push_frame() {
+    if (this.currentFrame >= this.frames.length) return
+    this.$el.currentTime = parseFloat(this.frames[this.currentFrame + 1])
+  }
+
+  back_frame() {
+    if (this.currentFrame <= 0) return
+    this.$el.currentTime = parseFloat(this.frames[this.currentFrame - 1])
+  }
+}
 
 ready(() => {
-  videojs.addLanguage('ja', Object.assign(lang_ja, {
-    'No compatible source was found for this media.': '現在エンコード処理中のため閲覧できません'
-  }))
-  const player = videojs('video-player')
+  const video = new Video('video')
+  doc('#prev-frame')[0].addEventListener('click', () => {
+    video.ready().back_frame()
+  })
+  doc('#next-frame')[0].addEventListener('click', () => {
+    video.ready().push_frame()
+  })
 })
