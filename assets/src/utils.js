dcf003309cd405a01ef1f1c3d598dfbe364aefe7
Compeito <com0806peito@icloud.com>
2019-03-16 16:35:24 +0900

    range追加

--- assets/src/utils.js
+++ assets/src/utils.js
@@ -1,3 +1,5 @@
 export const ready = callback => document.addEventListener('DOMContentLoaded', callback)
 
 export const doc = q => document.querySelectorAll(q)
+
+export const range = n => [...Array(n).keys()]
