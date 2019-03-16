17c5c5518ef2a8184754b9784e5b6cf5c6d46062
Compeito <com0806peito@icloud.com>
2019-03-16 23:38:26 +0900

    検索ページ調整

--- browse/views.py
+++ browse/views.py
@@ -10,6 +10,7 @@ from core.utils import AltPaginationListView
 class Home(AltPaginationListView):
     template_name = 'browse/index.html'
     context_object_name = 'videos'
+    paginate_by = 12
 
     def get_queryset(self):
         return safe_videos().order_by('-profile__created_at')
@@ -21,11 +22,11 @@ home = Home.as_view()
 class Search(AltPaginationListView):
     template_name = 'browse/search.html'
     context_object_name = 'videos'
-    q = ''
+    paginate_by = 12
 
     def get_queryset(self):
-        self.q = self.request.GET.get('q', '')
-        q_list = self.q.split(' ')
+        q = self.request.GET.get('q', '')
+        q_list = q.split(' ')
 
         return safe_videos().filter(
             functools.reduce(operator.and_, (Q(profile__title__contains=item) for item in q_list)) |
