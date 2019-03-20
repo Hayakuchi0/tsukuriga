8b72fde8fcc5d4de61272ac0465eb9c7f7b3347d
Compeito <com0806peito@icloud.com>
2019-03-20 23:51:25 +0900

    非ログイン時のエラーを修正

--- notify/context_processors.py
+++ notify/context_processors.py
@@ -1,4 +1,5 @@
 def notification(request):
-    return {
-        'has_new_notification': request.user.notification_set.filter(is_read=False).exists(),
-    }
+    has_new = False
+    if request.user.is_authenticated:
+        has_new = request.user.notification_set.filter(is_read=False).exists()
+    return {'has_new_notification': has_new}
