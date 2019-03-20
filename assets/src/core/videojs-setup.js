992dffd28a311aa265fb27205a26299bdc0c3beb
Compeito <com0806peito@icloud.com>
2019-03-20 16:28:29 +0900

    動画視聴ページを16x9に固定

--- assets/src/core/videojs-setup.js
+++ assets/src/core/videojs-setup.js
@@ -8,5 +8,7 @@ ready(() => {
   videojs.addLanguage('ja', Object.assign(lang_ja, {
     'No compatible source was found for this media.': '現在エンコード処理中のため閲覧できません'
   }))
-  const player = videojs('video-player')
+  const player = videojs('video-player', {
+    'aspectRatio': '16:9'
+  })
 })
