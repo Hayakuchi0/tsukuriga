7fa9f6e3d6d3ad6f92121dab7eb44c13d5a15cf2
Compeito <com0806peito@icloud.com>
2019-03-21 19:51:16 +0900

    feat: 説明追加

--- upload/forms.py
+++ upload/forms.py
@@ -9,10 +9,20 @@ class VideoFileUploadForm(forms.ModelForm):
 
 
 class VideoProfileForm(forms.ModelForm):
+    description = forms.CharField(
+        label='動画説明', widget=forms.Textarea(),
+        help_text='URLと@, #から始まる文字列はそれぞれリンク、ユーザーページへのリンク、ハッシュタグに置換されます'
+    )
+
     class Meta:
         model = VideoProfile
         fields = ('title', 'description')
 
 
 class VideoImportForm(forms.Form):
-    url = forms.URLField(label='URL')
+    url = forms.URLField(
+        label='URL', widget=forms.TextInput(attrs={'placeholder': 'https://...'}),
+        help_text="""次の形式のURLに対応しています: <br>
+twitter: https://twitter.com/{screen_name}/status/{tweet_id}<br>
+altwug: https://altwug.net/watch/{video_id}/<br>
+""")
