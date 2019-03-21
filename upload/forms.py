cbf66098c98bf67de297661af13b05e476143fba
Compeito <com0806peito@icloud.com>
2019-03-22 01:15:27 +0900

    fix: widgetを修正

--- upload/forms.py
+++ upload/forms.py
@@ -21,7 +21,7 @@ class VideoProfileForm(forms.ModelForm):
 
 class VideoImportForm(forms.Form):
     url = forms.URLField(
-        label='URL', widget=forms.TextInput(attrs={'placeholder': 'https://...'}),
+        label='URL', widget=forms.URLInput(attrs={'placeholder': 'https://...'}),
         help_text="""次の形式のURLに対応しています: <br>
 twitter: https://twitter.com/{screen_name}/status/{tweet_id}<br>
 altwug: https://altwug.net/watch/{video_id}/<br>
