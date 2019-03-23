ea3a3f5f7fe65b176f71348fd0759606c951be14
Compeito <com0806peito@icloud.com>
2019-03-23 19:09:32 +0900

    fix: アカウントページのページネーション適用

--- account/views.py
+++ account/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.views import LogoutView
 
 from .models import User
+from upload.models import Video
+from core.utils import AltPaginationListView
 
 
 class CustomLogoutView(LogoutView):
     next_page = '/'
 
 
-def profile(request, username):
-    account = get_object_or_404(User, username=username)
-    return render(request, 'users/profile.html', {'account': account})
+class Profile(AltPaginationListView):
+    template_name = 'users/profile.html'
+    context_object_name = 'videos'
+    paginate_by = 12
+
+    def get_queryset(self):
+        return Video.objects.filter(user__username=self.kwargs['username']).order_by('-profile__created_at')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['account'] = get_object_or_404(User, username=self.kwargs['username'])
+        return context
+
+
+profile = Profile.as_view()
