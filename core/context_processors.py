5218ff0e9f276157b027478f88a175b753e73a6c
Compeito <com0806peito@icloud.com>
2019-03-23 18:27:13 +0900

    fix: paginationと他パラメータの競合を解決

---
+++ core/context_processors.py
@@ -0,0 +1,7 @@
+def query_resolver(request):
+    """paginationと検索クエリなど他パラメータとの競合を解決する"""
+    queries = '?'
+    for k, v in dict(request.GET).items():
+        if not k == 'page':
+            queries += f'{k}={v[0]}&'
+    return {'queries': queries}
--- munikis/settings.py
+++ munikis/settings.py
@@ -91,6 +91,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.query_resolver',
                 'pages.context_processors.pages',
                 'notify.context_processors.notification',
             ],
