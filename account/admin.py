06634ab2d81b8c0c3be0c393944a3c7dc985defc
Compeito <com0806peito@icloud.com>
2019-04-03 23:14:29 +0900

    feat: トロフィーをユーザー管理ページに表示

--- account/admin.py
+++ account/admin.py
@@ -5,15 +5,19 @@ from django.utils.translation import ugettext_lazy as _
 from . import models
 
 
+class TrophyUserInline(admin.TabularInline):
+    model = models.TrophyUserRelation
+
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('name', 'email', 'description', 'profile_icon', 'profile_banner')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     list_display = ('username', 'email', 'name', 'is_staff')
+    inlines = (TrophyUserInline,)
 
 
 class TrophyAdmin(admin.ModelAdmin):
