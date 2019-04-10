4a1bbf7b2023d420a8fb3125f02f12f2ffbb153e
Compeito <com0806peito@icloud.com>
2019-04-10 23:26:33 +0900

    feat: submenuの文字列を記事画面から編集可能に

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -2,4 +2,12 @@ from .models import Page
 
 
 def pages(request):
-    return {'pages': Page.objects.filter(is_published=True).order_by('-created_at')[:5]}
+    try:
+        submenu_message = Page.objects.get(slug='submenu').body
+    except:
+        submenu_message = ''
+
+    return {
+        'pages': Page.objects.filter(is_published=True).order_by('-created_at')[:5],
+        'submenu_message': submenu_message
+    }
