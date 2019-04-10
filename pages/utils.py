07141b2b731ac27676e8abce7487abe0402296e3
Compeito <com0806peito@icloud.com>
2019-04-10 23:12:10 +0900

    feat: pymdown拡張用設定追加

--- pages/utils.py
+++ pages/utils.py
@@ -1,5 +1,22 @@
-from markdown import markdown
+import markdown
 
 
 def markdownify(content):
-    return markdown(content, tab_length=2)
+    return markdown.Markdown(
+        tab_length=2,
+        extensions=[
+            'toc',
+            'pymdownx.magiclink',
+            'pymdownx.tilde',
+        ],
+        extension_configs={
+            'pymdownx.magiclink': {
+                'hide_protocol': True,
+                'social_url_shorthand': True,
+                'repo_url_shortener': True,
+                'repo_url_shorthand': True,
+                'user': 'Compeito',
+                'repo': 'tsukuriga'
+            },
+        }
+    ).convert(content)
