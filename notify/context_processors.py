6368d676d0a6ac8c7105b6b4f8dfb2531262e540
Compeito <com0806peito@icloud.com>
2019-04-15 15:27:38 +0900

    fix: Notification変更に伴う修正

--- notify/context_processors.py
+++ notify/context_processors.py
@@ -1,5 +1,5 @@
 def notification(request):
     has_new = False
     if request.user.is_authenticated:
-        has_new = request.user.notification_set.filter(is_read=False).exists()
+        has_new = request.user.received_notifications.filter(is_read=False).exists()
     return {'has_new_notification': has_new}
