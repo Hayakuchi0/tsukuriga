3e9d5911b1d1ebd52ff879142dc13785e5e9ac2e
Compeito <com0806peito@icloud.com>
2019-04-21 18:36:52 +0900

    feat: 記事トップ表示のための調整

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -7,7 +7,11 @@ def pages(request):
     except:
         submenu_message = ''
 
+    latest_pages = Page.objects.filter(is_published=True).order_by('-created_at')
+    featured_pages = sorted([p for p in latest_pages if p.featured_order], key=lambda p: p.featured_order)
+
     return {
-        'pages': Page.objects.filter(is_published=True).order_by('-created_at')[:5],
+        'latest_pages': latest_pages[:5],
+        'featured_pages': featured_pages,
         'submenu_message': submenu_message
     }
