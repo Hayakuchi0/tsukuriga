a8aa3eec838621faec73e3d5aadfd6ab248ba45c
Compeito <com0806peito@icloud.com>
2019-04-22 01:32:05 +0900

    feat: swiper設置

--- assets/src/base/app.js
+++ assets/src/base/app.js
@@ -82,9 +82,12 @@ ready(() => {
    * Swiper
    */
   new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
     },
   })
 })
