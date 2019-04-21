2e3a36809975df5588feb943950b9c3ddd27ce1a
Compeito <com0806peito@icloud.com>
2019-04-21 23:20:38 +0900

    feat: swiper適用

--- assets/src/base/app.js
+++ assets/src/base/app.js
@@ -1,4 +1,5 @@
 import Tooltip from 'tooltip.js'
+import Swiper from 'swiper'
 
 import { doc, ready, Notify } from '../utils'
 import './styles.scss'
@@ -75,5 +76,15 @@ ready(() => {
       title: $tooltip.dataset.tooltip,
       placement: 'bottom',
     })
+  })
+
+  /**
+   * Swiper
+   */
+  new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   })
 })
