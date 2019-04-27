06d7d088780ca9f3ffc5e772903e8662fb2fcbd8
Compeito <com0806peito@icloud.com>
2019-04-27 23:52:26 +0900

    feat: 既存のDocker関連設定の削除

--- docker/debugDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-here=`dirname ${0}`/../
-here=`cd ${here};pwd`
-cd ${here}
-docker system prune
-docker run -v ${here}:/var/www/html -p 8000:8000 -it --name tsukuriga tsukuriga:dev /bin/bash
