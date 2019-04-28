de22bc94d3e983b63011574f721e032625a65270
Compeito <com0806peito@icloud.com>
2019-04-28 19:44:03 +0900

    fix: 集計中かどうかの判断を厳格化

--- browse/views.py
+++ browse/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 
 from .utils import safe_videos
 from .models import Ranking
+from upload.models import Video
 from core.utils import AltPaginationListView
 
 
@@ -54,7 +55,9 @@ class RankingList(AltPaginationListView):
         return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
-        return Ranking.objects.filter(type=self.ranking_type, day=self.ranking_day).order_by('-point')
+        rankings = Ranking.objects.filter(type=self.ranking_type, day=self.ranking_day).order_by('-point')
+        videos = Video.objects.all()
+        return rankings if rankings.count() >= videos.count() else []
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
