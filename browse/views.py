eb20221fad8491b42ad9c76441acb53de9ac310a
Compeito <com0806peito@icloud.com>
2019-04-30 01:14:01 +0900

    feat: チャンネル用ビュー追加

--- browse/views.py
+++ browse/views.py
@@ -1,10 +1,11 @@
 import functools
 import operator
 
+from django.shortcuts import get_object_or_404
 from django.db.models import Q
 
 from .utils import safe_videos
-from .models import Ranking
+from .models import Ranking, Channel
 from core.utils import AltPaginationListView
 
 
@@ -63,3 +64,26 @@ class RankingList(AltPaginationListView):
 
 
 ranking = RankingList.as_view()
+
+
+class ChannelList(AltPaginationListView):
+    template_name = 'browse/channel.html'
+    context_object_name = 'videos'
+    paginate_by = 12
+
+    ch = None
+
+    def get(self, request, *args, **kwargs):
+        self.ch = get_object_or_404(Channel, number=self.kwargs.get('number'))
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return safe_videos().filter(profile__channels=self.ch)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['channel'] = self.ch
+        return context
+
+
+channel = ChannelList.as_view()
