d2af9d4e2b49eb5bb63a1c020f09d516946cd242
Compeito <com0806peito@icloud.com>
2019-04-30 00:01:27 +0900

    feat: 編集フォームにチャンネル編集項目追加

--- upload/generic.py
+++ upload/generic.py
@@ -1,21 +1,27 @@
 from django.shortcuts import redirect
 from django.contrib import messages
-from django.views.generic import UpdateView
 
-from .forms import VideoProfileForm
+from extra_views import UpdateWithInlinesView
+
+from .models import VideoProfile
+from .forms import VideoProfileForm, ChannelInline
 from ajax.models import Comment
 
 
-class VideoProfileUpdateView(UpdateView):
+class VideoProfileUpdateView(UpdateWithInlinesView):
+    model = VideoProfile
     form_class = VideoProfileForm
-    pk_url_kwarg = 'slug'
+    inlines = [ChannelInline]
 
     def get_object(self, queryset=None):
         return self.request.video.profile
 
-    def form_valid(self, form):
+    def forms_valid(self, form, inlines):
         video = self.request.video
         form.save()
+        for formset in inlines:
+            formset.save()
+
         if not video.profile.allows_anonymous_comment:
             comments = Comment.objects.filter(video=video, is_anonymous=True)
             for comment in comments:
@@ -27,3 +33,11 @@ class VideoProfileUpdateView(UpdateView):
 
         messages.success(self.request, '保存されました')
         return redirect(self.get_success_url())
+
+    def forms_invalid(self, form, inlines):
+        # クラス名が書かれたようなやつが出るので、汎用的なメッセージで上書きしたい
+        for formset in inlines:
+            for error in formset.errors:
+                if '__all__' in error.keys():
+                    error['__all__'] = ['指定した値が不正です']
+        return super().forms_invalid(form=form, inlines=inlines)
