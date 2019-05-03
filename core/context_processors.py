071b01b745bfc667c46a972d3edb05b4cfc3817d
Compeito <com0806peito@icloud.com>
2019-05-03 12:48:12 +0900

    refactor: page以外のGETクエリが無い場合の処理を変更

--- core/context_processors.py
+++ core/context_processors.py
@@ -7,7 +7,7 @@ def query_resolver(request):
     for k, v in dict(request.GET).items():
         if not k == 'page':
             queries += f'{k}={v[0]}&'
-    return {'queries': queries}
+    return {'queries': queries if not queries == '?' else ''}
 
 
 def common(request):
