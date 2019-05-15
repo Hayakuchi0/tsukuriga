729d020fcf4e161ca40f1a21a758057bd327f669
Compeito <com0806peito@icloud.com>
2019-05-15 21:44:08 +0900

    fix: targetがNoneになっているものを除外

--- notify/context_processors.py
+++ notify/context_processors.py
@@ -1,5 +1,7 @@
 def notification(request):
     has_new = False
     if request.user.is_authenticated:
-        has_new = request.user.received_notifications.filter(is_read=False).exists()
+        # target=Noneはメール連投対策のために削除せず、除外することで対応する
+        not_reads = request.user.received_notifications.filter(is_read=False)
+        has_new = bool([n for n in not_reads if n.target])
     return {'has_new_notification': has_new}
