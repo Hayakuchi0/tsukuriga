3118da89ba82e5dd06c0d779ce9d69ce291e7d44
Compeito <com0806peito@icloud.com>
2019-05-27 15:46:04 +0900

    feat: 再投稿用ビュー追加

--- upload/urls.py
+++ upload/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.upload),
     path('/import', views.import_upload),
+    path('/update/<slug:slug>', views.update),
     path('/detail/<slug:slug>', views.detail),
     path('/complete/<slug:slug>', views.complete)
 ]
