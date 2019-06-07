ccedae5260ce078ba1e47428e03ecc067433c672
Compeito <com0806peito@icloud.com>
2019-06-08 00:30:59 +0900

    feat: Postモデル追加

--- tools/models.py
+++ tools/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core.utils import CustomModel
 
-# Create your models here.
+
+class Post(CustomModel):
+    name = models.CharField('名前', max_length=20)
+    text = models.TextField('本文', max_length=140)
+
+    def __str__(self):
+        return f'{self.text}({self.name})'
