e3e31c8a1c80db76b8e0b22bcf4f9dbd5627830d
Compeito <com0806peito@icloud.com>
2019-06-15 20:56:48 +0900

    fix: gif未生成ファイル取得方法を修正

--- upload/management/commands/gif.py
+++ upload/management/commands/gif.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from upload.models import VideoData
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for video in VideoData.objects.filter(gif__isnull=True, duration__gte=3):
+        for video in VideoData.objects.filter(Q(gif__isnull=True) | Q(gif__exact=''), duration__gte=3):
             video.update_gif()
