ce83c3692d0bcc1c3e71bceb90129ca59889e588
Compeito <com0806peito@icloud.com>
2019-06-23 15:35:49 +0900

    feat: トップページにピックアップ作品表示

--- browse/views.py
+++ browse/views.py
@@ -14,7 +14,9 @@ def home(request):
     recent_videos = safe_videos().order_by('-published_at')[:50]
     recent_videos = sorted(recent_videos, key=lambda x: random.random())[:8]
     top_rankings = RankingList().get_queryset()[:8]
-    return render(request, 'browse/index.html', {'recent_videos': recent_videos, 'top_rankings': top_rankings})
+    pickup_videos = safe_videos().filter(is_pickup=True).order_by('-published_at')[:6]
+    return render(request, 'browse/index.html',
+                  {'recent_videos': recent_videos, 'top_rankings': top_rankings, 'pickup_videos': pickup_videos})
 
 
 class Recent(AltPaginationListView):
