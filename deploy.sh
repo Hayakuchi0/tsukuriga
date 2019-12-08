6314a11dc371c14eb9d112ee692338f7f22eff01
Compeito <com0806peito@icloud.com>
2019-12-08 13:19:38 +0900

    build: deploy.sh更新

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
+#!/bin/sh
+cd `dirname $0`
 docker-compose down
+
+# イメージの再ビルド
+docker-compose build
+
+# 依存関係の更新とDBマイグレーション
+docker-compose run web bash -c "pipenv install --ignore-pipfile && python manage.py migrate"
+
+# js/cssの再ビルドと配置
 rm -rf nginx/assets/bundles/*
 rm -rf nginx/assets/webpack-stats.json
 docker-compose run node bash -c "yarn && yarn run build"
 docker-compose run web python manage.py collectstatic --no-input
+
 docker-compose up -d
