14cabae7668032845d099fedcd8d20c347ba7c6e
Compeito <com0806peito@icloud.com>
2019-12-09 22:32:52 +0900

    fix: iOSでPWAを配信しないよう変更

--- web/core/context_processors.py
+++ web/core/context_processors.py
@@ -1,3 +1,4 @@
+import re
 import urllib.parse
 
 from django.conf import settings
@@ -12,8 +13,16 @@ def query_resolver(request):
     return {'queries': queries}
 
 
+def user_agent_detect(request):
+    ua = request.META['HTTP_USER_AGENT'] if 'HTTP_USER_AGENT' in request.META.keys() else ''
+    return {
+        'is_ios': len(re.findall('(iphone|ipad|ipod)', ua, re.I)) > 0
+    }
+
+
 def common(request):
     return {
         'DEBUG': settings.DEBUG,
-        **query_resolver(request)
+        **query_resolver(request),
+        **user_agent_detect(request)
     }
